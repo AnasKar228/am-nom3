1477f60f003045afcb5077fee5cf868aceeb60fa
AnasKar228 <anastarkar@gmail.com>
2026-05-22 14:59:48 +0300

    mama model

--- project/src/models/bert_model.py
+++ project/src/models/bert_model.py
@@ -89,6 +89,7 @@ def train_bert(
         per_device_eval_batch_size=config.get("per_device_eval_batch_size", 64),
         learning_rate=config.get("learning_rate", 2e-5),
         weight_decay=0.01,
+        warmup_ratio=config.get("warmup_ratio", 0.1),
         eval_strategy="epoch",
         save_strategy="epoch",
         load_best_model_at_end=True,
